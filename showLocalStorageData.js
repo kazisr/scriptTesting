36b7e3c324ca34b3368246d08b2316d7915de7f6
Kazi Shahrier Rafid <77842274+kazisr@users.noreply.github.com>
2024-05-28 16:18:43 +0600

    Update showLocalStorageData.js

--- showLocalStorageData.js
+++ showLocalStorageData.js
@@ -1,16 +1,3 @@
- // Check if local storage is supported by the browser
-    if (typeof(Storage) !== "undefined") {
-        // Retrieve data from local storage
-        var storedData = localStorage.getItem("myData");
-
-        // Check if data exists in local storage
-        if (storedData !== null) {
-            // Display the retrieved data
-            console.log("Data stored in local storage:");
-            console.log(storedData);
-        } else {
-            console.log("No data found in local storage.");
-        }
-    } else {
-        console.log("Local storage is not supported by this browser.");
-    }
+window.onload = function() {
+    alert('This is an alert message that appears when the page loads!');
+};
